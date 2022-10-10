bda22db079ba97173a047c0b89524e90bbf56f47
snorkypie <>
2022-10-10 21:46:39 +0300

    Minor cleanup

--- test/verify.ts
+++ test/verify.ts
@@ -1,16 +1,15 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-const { hexlify, toUtf8Bytes } = ethers.utils;
 
 describe("Verify", function () {
   async function deploy() {
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [owner] = await ethers.getSigners();
 
     const Verify = await ethers.getContractFactory("Verify");
     const verify = await Verify.deploy();
 
-    return { verify, owner, otherAccount };
+    return { verify, owner };
   }
 
   it("it can verify signature", async function () {
